acc9d7082090c1abc65f50a2bdd9c82278b63d75
SalimSleem <salimwslim@gmail.com>
2023-10-11 09:29:43 +0300

    init

---
+++ footer.js
@@ -0,0 +1,5 @@
+document.write('<div class="container px-5">\
+    <div class="row align-items-center justify-content-between flex-column flex-sm-row">\
+        <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Phoenician Studios 2023</div></div>\
+    </div>\
+</div>');
---
+++ menu.js
@@ -0,0 +1,12 @@
+document.write('\
+    <div class="container px-5">\
+        <a class="navbar-brand" href="index.html" style="margin-right:5px;font-family:Cheddarcake;"><image src="images/Boat Transparent.png" style="width:5%; padding-bottom:1%; padding-right:1%">Phoenician Studios</a>\
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>\
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">\
+                <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>\
+                <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>\
+                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>\
+            </ul>\
+        </div>\
+    </div>');
